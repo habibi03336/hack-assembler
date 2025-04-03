b99290e66fe0d3e906961666050606bfd6087dc5
하지훈 <jnh03336@gmail.com>
2025-04-03 21:36:10 +0900

    feat: pop command 변환 로직

--- src/main/java/org/example/vmTranslator/CodeWriter.java
+++ src/main/java/org/example/vmTranslator/CodeWriter.java
@@ -1,5 +1,7 @@
 package org.example.vmTranslator;
 
+import java.util.Map;
+
 /*
 * writes the assembly code that implements that parsed command
 * */
@@ -7,9 +9,30 @@ public class CodeWriter {
 
     public CodeWriter(){}
 
-    public void writeArithmetic(String command){}
+    public void writeArithmetic(String arithmetic){}
 
     public void writePushPop(String command, String segment, int index){}
+
+    private Map<String, String> segmentMapping = Map.of(
+            "local", "LCL"
+    );
+    String[] translatePop(String segment, int index){
+        return new String[]{
+                "@SP",
+                "M=M-1",
+                "A=M",
+                "D=M",
+                "@" + segmentMapping.get(segment),
+                "D=D+M",
+                String.format("@%d", index),
+                "D=D+A",
+                "@SP",
+                "A=M",
+                "A=M",
+                "A=D-A",
+                "M=D-A"
+        };
+    }
 
     public void close(){}
 
--- src/test/java/org/example/vmTranslator/CodeWriterTest.java
+++ src/test/java/org/example/vmTranslator/CodeWriterTest.java
@@ -1,7 +1,34 @@
 package org.example.vmTranslator;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CodeWriterTest {
+
+    @Test
+    void translatePopCommandTest(){
+        // given
+        CodeWriter codeWriter = new CodeWriter();
+
+        // execute
+        String[] translated = codeWriter.translatePop("local", 7);
+
+        // then
+        // https://evoniuk.github.io/posts/nand.html ... beautiful optimization, mine was 17 lines.
+        assertEquals("@SP", translated[0]);
+        assertEquals("M=M-1", translated[1]);
+        assertEquals("A=M", translated[2]);
+        assertEquals("D=M", translated[3]);
+        assertEquals("@LCL", translated[4]);
+        assertEquals("D=D+M", translated[5]);
+        assertEquals("@7", translated[6]);
+        assertEquals("D=D+A", translated[7]);
+        assertEquals("@SP", translated[8]);
+        assertEquals("A=M", translated[9]);
+        assertEquals("A=M", translated[10]);
+        assertEquals("A=D-A", translated[11]);
+        assertEquals("M=D-A", translated[12]);
+    }
 
 }
